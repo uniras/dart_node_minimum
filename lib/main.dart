a310a2fc1351265980044ff3a3d4efe7fcfade68
uniras <tkappeng@gmail.com>
2022-10-24 06:15:42 +0900

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'dart:js';
+import 'package:js/js.dart';
+import 'package:js/js_util.dart';
+import 'package:node_interop/node.dart';
+
+void main() {
+  //print("Hello world, I'm currently in ${process.cwd()}.");
+}
